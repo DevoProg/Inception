2eea66cb3a06affb65bf3983521a9f62904da195
Amaury Devos <adevos@fu-r10-p2.s19.be>
2023-10-17 12:40:09 +0200

    ajustements

--- srcs/requirements/mariadb/conf/createdb.sql
+++ srcs/requirements/mariadb/conf/createdb.sql
@@ -5,7 +5,7 @@ DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 
 /** On a choisi de l installer avec la methode d authentification normale donc il faut set le password **/
-/*SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');*/
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');
 
 /** Creation du premier user, l'autre sera cree via le container wordpress */
 CREATE DATABASE $MARIADB_DB;
